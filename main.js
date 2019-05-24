d703ebd1f30c50f5eb7cb97c877d11707424ae0f
Isabel <isabel.antonia.seidl@gmail.com>
2019-05-24 17:57:12 +0200

    menu for small screens

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
+// everything menu related 
 document.getElementById("hamburger").addEventListener('click', openMenu)
+
+document.getElementById("dropDown").addEventListener('click', openSubMenu)
 
 function openMenu() {
     $("#menuBody").toggleClass("show");
 }
+
+function openSubMenu() {
+    event.preventDefault();
+    $("#subMenu").toggleClass("show");
+
+}
+
+
+
+
+$(window).on("scroll", function () {
+
+
+
+    if ($(window).scrollTop() > ($('#menuHeader').outerHeight())) {
+        $("#menuHeader").addClass("fixed");
+        $("#menuBody").addClass("fixMenu");
+
+    }
+
+    if ($(window).scrollTop() <= ($('#menuHeader').outerHeight())) {
+        $("#menuHeader").removeClass("fixed");
+        $("#menuBody").removeClass("fixMenu");
+    }
+
+});
